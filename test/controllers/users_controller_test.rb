850e1871f99402301e5d413ea1c1c03d181f46a1
averpavel <averpavel@gmail.com>
2016-12-02 09:28:25 +0100

    add user model validation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,18 +15,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_user_url
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, name: @user.name } }
-    end
-
-    assert_redirected_to user_url(User.last)
-  end
 
   test "should show user" do
     get user_url(@user)
@@ -38,10 +26,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, name: @user.name } }
-    assert_redirected_to user_url(@user)
-  end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
