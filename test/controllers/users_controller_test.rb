d991041cd604f542af88daa3955f655609508adb
averpavel <averpavel@gmail.com>
2016-12-09 09:04:41 +0100

    users uniqueness validation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,27 +10,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get index" do
-    get users_url
-    assert_response :success
-  end
 
 
-  test "should show user" do
-    get user_url(@user)
-    assert_response :success
-  end
 
-  test "should get edit" do
-    get edit_user_url(@user)
-    assert_response :success
-  end
-
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user)
-    end
 
     assert_redirected_to users_url
   end
