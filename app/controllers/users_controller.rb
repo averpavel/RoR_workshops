6d4d19f7f2345fd8001bd826794f96731a16497e
averpavel <averpavel@gmail.com>
2017-01-13 09:20:56 +0100

    successfull sign-up test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
       if @user.save
+        flash[:success] = "Welcome to the Awesome App!"
         redirect_to @user
       else
         render 'new'
